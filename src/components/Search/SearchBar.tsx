b54882c8e9a9346dd5f4d1de02d67115375fbd6b
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-11-01 19:28:19 -0300

    Adding card info

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -2,6 +2,12 @@ import axios from 'axios';
 import '../../index.css'
 import { useEffect, useState } from 'react';
 
+interface UniversityDetails {
+  name: string;
+  country: string;
+  web_pages: string;
+}
+
 export function SearchBar() {
   const [country, setCountry] = useState("");
   const [universityName, setUniversityName] = useState("");
@@ -9,6 +15,8 @@ export function SearchBar() {
   const [universitySuggestions, setUniversitySuggestions] = useState<string[]>(
     []
   );
+  const [universityDetails, setUniversityDetails] =
+    useState<UniversityDetails | null>(null);
 
     useEffect(() => {
       loadCountrySuggestions();
@@ -44,62 +52,88 @@ export function SearchBar() {
       const response = await axios.get(
         `http://universities.hipolabs.com/search?name=${universityName}&country=${country}`
       );
-      const univesities = response.data
+      const univesities: UniversityDetails[] = response.data;
+      
+      if (univesities.length > 0) {
+        setUniversityDetails(univesities[0]);
+      } else {
+        setUniversityDetails(null);
+      }
+
       console.log(univesities)
     } catch (error) {
       console.error("Request error!", error);
     }
   }
 
+  console.log(universityDetails?.name)
   return (
-    <div className="flex items-center">
-      <div className="flex space-x-1">
-        <input
-          type="text"
-          value={country}
-          onChange={(e) => setCountry(e.target.value)}
-          className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-          placeholder="Country"
-          list="country-suggestions"
-        />
-        <datalist id="country-suggestions">
-          {countrySuggestions.map((suggestion, index) => (
-            <option key={index} value={suggestion} />
-          ))}
-        </datalist>
-        <input
-          type="text"
-          value={universityName}
-          onChange={(e) => setUniversityName(e.target.value)}
-          className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-          placeholder="University Name"
-          list="university-suggestions"
-        />
-        <datalist id="university-suggestions">
-          {universitySuggestions.map((suggestion, index) => (
-            <option key={index} value={suggestion} />
-          ))}
-        </datalist>
-        <button
-          onClick={handleSearch}
-          className="px-4 text-white bg-[#3f3cbb] rounded-full "
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-5 h-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
+    <section>
+      <div className="flex items-center">
+        <div className="flex space-x-1">
+          <input
+            type="text"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            placeholder="Country"
+            list="country-suggestions"
+          />
+          <datalist id="country-suggestions">
+            {countrySuggestions.map((suggestion, index) => (
+              <option key={index} value={suggestion} />
+            ))}
+          </datalist>
+          <input
+            type="text"
+            value={universityName}
+            onChange={(e) => setUniversityName(e.target.value)}
+            className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+            placeholder="University Name"
+            list="university-suggestions"
+          />
+          <datalist id="university-suggestions">
+            {universitySuggestions.map((suggestion, index) => (
+              <option key={index} value={suggestion} />
+            ))}
+          </datalist>
+          <button
+            onClick={handleSearch}
+            className="px-4 text-white bg-[#3f3cbb] rounded-full "
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
-    </div>
+      <div className="flex justify-center mt-10">
+        {universityDetails && (
+          <div className="flex items-center flex-col bg-indigo-700 rounded p-5 text-white">
+            <h2>University Details</h2>
+            <p>Name: {universityDetails.name}</p>
+            <p>Country: {universityDetails.country}</p>
+            <a
+              className="bg-white text-black font-bold py-2 px-4 rounded mt-2"
+              href={universityDetails.web_pages}
+            >
+              Web site
+            </a>
+            {/* <img src={universityDetails.web_pages} alt="" /> */}
+          </div>
+        )}
+      </div>
+    </section>
   );
 }
